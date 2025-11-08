9eb712f11e83139a336bb3fc089ccdd87f8ecbb1
Arian Tapia <u202116567@upc.edu.pe>
2025-11-08 15:59:50 -0500

    feat(courses): vista individual de curso

---
+++ app/(home)/courses/[course]/page.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+
+export default async function CoursePage({
+  params,
+}: {
+  params: { course: string };
+}) {
+  const { course: courseId } = await params;
+  return (
+    <>
+      <img
+        src={`https://cdn.pixabay.com/index/2025/01/30/05-35-40-326_1440x550.png`}
+        alt="Nombre del Curso"
+        className="max-h-[30vh] w-full"
+      />
+      <div className="flex gap-30">
+        <div className="w-2/3 flex flex-col gap-5">
+          <h1 className="block text-4xl font-bold">Qué es SQL</h1>
+          <p className="text-justify">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae
+            vel sint corrupti sit a ducimus minima quidem doloribus saepe
+            delectus facilis voluptatibus sapiente odio maiores ipsum at nisi,
+            tempore iusto accusamus adipisci dicta distinctio? Possimus,
+            laboriosam qui facilis odit esse provident dignissimos enim vitae
+            ipsam quo facere accusamus explicabo sit.
+          </p>
+
+          <Link href={`/courses/${courseId}/lesson/1`}>
+            <button className="bg-blue-900 text-white py-2 px-4 rounded cursor-pointer hover:bg-blue-700">
+              Comenzar Curso
+            </button>
+          </Link>
+        </div>
+        <ul className="w-1/3 flex flex-col gap-5">
+          <li>Lección 1: Introducción a SQL</li>
+          <li>Lección 2: Historia de SQL</li>
+          <li>Lección 3: Importancia en el mundo real</li>
+          <li>Lección 4: Bases de datos</li>
+          <li>Lección 5: Conocimientos Previos</li>
+        </ul>
+      </div>
+    </>
+  );
+}
